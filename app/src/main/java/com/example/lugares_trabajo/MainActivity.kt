d14a997a10e8a152828ec703ab35ca3b274843b3
CHUCM03 <albertojesus0301@gmail.com>
2022-10-19 21:31:31 -0600

    login no completado

--- app/src/main/java/com/example/lugares_trabajo/MainActivity.kt
+++ app/src/main/java/com/example/lugares_trabajo/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.lugares_trabajo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.lugares_trabajo.databinding.ActivityMainBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        FirebaseApp.initializeApp(this)
+        auth = Firebase.auth
+
+        binding.btLogin.setOnClickListener(){
+            hacerlogin();
+        }
+        binding.btRegister.setOnclickListener(){
+            hacerRegister();
+        }
+
     }
-}
+
+private fun hacerRegister(){
+    var email = binding.etMail.text.toString()
+    var clave = binding.etClave.text.toString()
+
+    auth.createUserWithEmailAndPassword(email,clave)
+        .addOnCompleteListener(this) { task ->
+            if(task.isSuccessful){
+
+            }
+        }
+
